a4c1b39ecf295073c48fa4e8d6d6b9b88a0fa15e
graphicspoint <graphicspoint29@gmail.com>
2024-01-11 21:22:42 +0530

    table migration

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'body',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'deleted_at'
+    ];
+}
